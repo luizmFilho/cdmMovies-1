4e42f1286b91391c64b5c8db2f81ed242801ee75
Giovane Oliveira de Barcelos <giovane_barcelos@uniritter.local>
2020-03-05 21:57:44 -0300

    Import HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'pages/home_page.dart';
+
 void main() => runApp(MoviesAPP());
 
 class MoviesAPP extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return HomePage();
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Filmes"),
-          centerTitle: true,
-        ),
-        body: Container(
-            color: Colors.white,
-            child: Text("Filmes em Cartaz",
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ))),
-      ),
-    );
   }
 }
