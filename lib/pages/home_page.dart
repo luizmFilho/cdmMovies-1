e4783b1482ef3c31d43352c7c3202a7583cd67be
Giovane Oliveira de Barcelos <giovane_barcelos@uniritter.local>
2020-03-12 22:03:14 -0300

    Acionamento dos botões

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'page_1.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,17 +15,84 @@ class HomePage extends StatelessWidget {
           title: Text("Filmes"),
           centerTitle: true,
         ),
-        body: _body(),
+        body: _body(context),
       ),
     );
   }
 
-  Container _body() {
-    return Container(
-      color: Colors.white,
-      child: Image.asset("assets/images/movie1.jpeg"),
+  _body(context) {
+    return SingleChildScrollView(
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            _text(),
+            _pageView(),
+            _buttons(context),
+          ],
+        ),
+      ),
     );
   }
+
+  Column _buttons(context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _button(
+                context, "Page 1", () => _onClickNavigator(context, Page1())),
+            _button(context, "Page 2", null),
+            _button(context, "Page 3", null),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _button(context, "Snack", null),
+            _button(context, "Toast", null),
+            _button(context, "Dialog", null),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Container _pageView() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 350,
+      child: PageView(
+        children: <Widget>[
+          _img("movie1.jpeg"),
+          _img("movie2.jpeg"),
+          _img("movie3.jpeg"),
+          _img("movie4.jpeg"),
+          _img("movie5.jpeg"),
+        ],
+      ),
+    );
+  }
+
+  void _onClickNavigator(BuildContext context, Widget page) {
+    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
+      return page;
+    }));
+  }
+
+  RaisedButton _button(context, String str, Function onPressed) {
+    return RaisedButton(
+      color: Colors.blue,
+      child: Text(str),
+      onPressed: onPressed,
+    );
+  }
+
+  Image _img(String movie) => Image.asset(
+        "assets/images/" + movie,
+        fit: BoxFit.cover,
+      );
 
   Text _text() {
     return Text("Filmes em Cartaz",
---
+++ lib/pages/page_1.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Página 1"),
+      ),
+    );
+  }
+}
